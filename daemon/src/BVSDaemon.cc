6e46ee76316abcf998cd82b287aaca3fd20021b6
nilsonholger <nilsonholger@hyve.org>
2012-04-17 19:39:09 +0200

    daemon: add tests for config

--- daemon/src/BVSDaemon.cc
+++ daemon/src/BVSDaemon.cc
@@ -10,15 +10,14 @@ int main(int argc, char** argv)
 
     //bvs.setup(); // load and apply options from config
 
-    //BVSConfig config("BVSDaemon");
-
     BVSLogger logger("BVSDaemon");
-    LOG(2, "Starting!");
+    LOG(2, "starting!");
 
-    //bvs.loadModules();
+    // TODO make it load all modules
+    bvs.loadModules();
 
 
-    /*
+    // TODO either cleanup or turn into conditional test
     LOG(0, "CLI");
     LOG(0, "CLI FILE");
     bvs.disableLogConsole();
@@ -39,24 +38,39 @@ int main(int argc, char** argv)
     bvs.enableLogConsole();
     BVSLogger both("BOTH", 3, BVSLogger::TO_CLI_AND_FILE);
     both.out(0) << "CLI AND FILE" << std::endl;
-    */
 
 
-    /*
     int foo;
     std::string bar;
-    config.getValue("f,foo", foo)
-        .getValue("bar,b", bar);
+    bool test;
+    bvs.config.getValue("core.foo", foo)
+        .getValue("core.bar", bar)
+        .getValue("core.test", test);
+
     LOG(0, "Getting foo: " << foo);
     LOG(0, "Getting bar: " << bar);
-    int foo2 = config.getValue<int>("f,foo");
-    LOG(0, "Getting long: " << foo2);
-    config.getValue("long", foo2);
-    LOG(0, "Getting long: " << foo2);
-    config.getValue("s", foo2);
-    LOG(0, "Getting s: " << foo2);
-    LOG(0, "BOOL: " << true);
-    */
+    LOG(0, "BOOL: " << test);
+
+    foo = bvs.config.getValue<int>("core.foo");
+    bar = bvs.config.getValue<std::string>("core.bar");
+    test = bvs.config.getValue<bool>("core.test");
+    LOG(0, "Getting foo directly: " << foo);
+    LOG(0, "Getting bar directly: " << bar);
+    LOG(0, "BOOL directly: " << test);
+
+    std::string bar2;
+    std::string bar3;
+    bvs.config.getValue("core.bar", bar2);
+    bar3 = bvs.config.getValue<std::string>("core.bar");
+    LOG(0, "bar2: " << bar2);
+    LOG(0, "bar3: " << bar3);
+
+    bool test2;
+    bool test3;
+    bvs.config.getValue("core.test", test2);
+    test3 = bvs.config.getValue<bool>("core.test");
+    LOG(0, "test2: " << test2);
+    LOG(0, "test3: " << test3);
 
     return 0;
 }
