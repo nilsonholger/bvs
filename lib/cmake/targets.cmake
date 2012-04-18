09c588b2ff7176df29712b1f6f5b1ac7d64cb3e9
nilsonholger <nilsonholger@hyve.org>
2012-04-18 15:54:29 +0200

    cmake: fix todo target

--- lib/cmake/targets.cmake
+++ lib/cmake/targets.cmake
@@ -1,2 +1,2 @@
 add_custom_target(run ./run working_diretory ${CMAKE_SOURCE_DIR})
-add_custom_target(todo which ack && ack --type=nomake TODO || grep -Hrn TODO *)
+add_custom_target(todo which ack && ack --type=cpp TODO || grep -Hrn TODO *)
