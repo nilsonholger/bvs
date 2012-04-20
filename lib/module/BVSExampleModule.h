2823b835b596008a428d4c2fdd0ea6989da08f24
nilsonholger <nilsonholger@hyve.org>
2012-04-20 19:35:39 +0200

    module: add example module

---
+++ lib/module/BVSExampleModule.h
@@ -0,0 +1,28 @@
+#ifndef BVSEXAMPLEMODULE_H
+#define BVSEXAMPLEMODULE_H
+
+#include "BVSModule.h"
+
+// TODO add comments to explain usage and give examples !!!IMPORTANT
+class BVSExampleModule : public BVSModule
+{
+    public:
+        BVSExampleModule(BVSConfig& config);
+        ~BVSExampleModule();
+        void onLoad();
+        void preExecute();
+        void execute();
+        void postExecute();
+        void debugDisplay();
+        void onClose();
+
+    private:
+        std::string identifier;
+        BVSLogger logger;
+        BVSConfig& config;
+        BVSExampleModule(const BVSExampleModule&) = delete; /**< -Weffc++ */
+        BVSExampleModule& operator=(const BVSExampleModule&) = delete; /**< -Weffc++ */
+};
+
+#endif //BVSEXAMPLEMODULE_H
+
