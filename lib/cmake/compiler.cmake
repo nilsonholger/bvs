1fc908e4066ca1a3978337feb6ae8eec4c327172
nilsonholger <nilsonholger@hyve.org>
2012-04-21 15:41:21 +0200

    cmake: move compiler stuff to toolbox

--- lib/cmake/compiler.cmake
+++ lib/cmake/compiler.cmake
@@ -1,19 +1,22 @@
 cmake_minimum_required(VERSION 2.8.6)
 
-option(USE_CLANG "build using clang" OFF)
-if(USE_CLANG)
-    SET (CMAKE_C_COMPILER             "clang")
-    SET (CMAKE_C_FLAGS                "-Wall -std=c99")
-    SET (CMAKE_C_FLAGS_DEBUG          "-g")
-    SET (CMAKE_C_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
-    SET (CMAKE_C_FLAGS_RELEASE        "-O4 -DNDEBUG")
-    SET (CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
 
-    SET (CMAKE_CXX_COMPILER             "clang++")
-    SET (CMAKE_CXX_FLAGS                "-Wall -std=c++11 -Wc++11-extensions")
-    SET (CMAKE_CXX_FLAGS_DEBUG          "-g")
-    SET (CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
-    SET (CMAKE_CXX_FLAGS_RELEASE        "-O4 -DNDEBUG")
-    SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
-endif(USE_CLANG)
 
+# STANDARDS
+set(CMAKE_C_FLAGS  "${CMAKE_C_FLAGS} -std=c99")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+if(${CMAKE_CXX_COMPILER} MATCHES "clang")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wc++11-extensions")
+endif(${CMAKE_CXX_COMPILER} MATCHES "clang")
+
+
+
+# WARNINGS
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Weffc++ -pedantic -pedantic-errors")
+
+
+
+# PROFILING
+if(USE_GPROF)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pg")
+endif(USE_GPROF)
