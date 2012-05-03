e87fc21d3deece4b145bba34a8e7ab9a380ccb69
nilsonholger <nilsonholger@hyve.org>
2012-05-03 19:18:02 +0200

    daemon: disable log

--- daemon/src/BVSDaemon.cc
+++ daemon/src/BVSDaemon.cc
@@ -5,7 +5,8 @@ int main(int argc, char** argv)
 {
     // start BVS
     BVS bvs(argc, argv);
-    bvs.enableLogFile("BVSLog.txt");
+    //bvs.enableLogFile("BVSLog.txt");
+    //bvs.enableLogConsole();
 
     BVSLogger logger("BVSDaemon");
     LOG(2, "starting!");
