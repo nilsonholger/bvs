f02ad6a977099b739caf99ae8bc6cfe70245e73f
nilsonholger <nilsonholger@hyve.org>
2013-04-12 12:53:55 +0200

    module: update/improve documentation

--- lib/module/BVSExampleModule.cc
+++ lib/module/BVSExampleModule.cc
@@ -5,16 +5,17 @@
 // This is your module's constructor.
 // Please do not change its signature as it is called by the framework (so the
 // framework actually creates your module) and the framework assigns the unique
-// identifier and gives you access to its config.
-// However, you should use it to create your data structures etc.
+// identifier and gives you access to configuration data.
 BVSExampleModule::BVSExampleModule(BVS::ModuleInfo info, const BVS::Info& bvs)
 	: BVS::Module(),
 	info(info),
 	logger(info.id),
 	bvs(bvs)
-	//yourSwitch(bvs.config.getValue<bool>(info.conf + ".yourSwitch, false)),
-	//input("testIn", BVS::ConnectorType::INPUT),
-	//output("testOut", BVS::ConnectorType::OUTPUT)
+	//input("inputName", BVS::ConnectorType::INPUT),
+	//output("outputName", BVS::ConnectorType::OUTPUT)
+
+	// CONFIGURATION RETRIEVAL
+	//yourSwitch(bvs.config.getValue<bool>(info.conf + ".yourSwitch", false)),
 {
 
 }
@@ -22,7 +23,6 @@ BVSExampleModule::BVSExampleModule(BVS::ModuleInfo info, const BVS::Info& bvs)
 
 
 // This is your module's destructor.
-// See the constructor for more info.
 BVSExampleModule::~BVSExampleModule()
 {
 
@@ -33,34 +33,20 @@ BVSExampleModule::~BVSExampleModule()
 // Put all your work here.
 BVS::Status BVSExampleModule::execute()
 {
-	// to log messages to console or file, use the LOG(...) macro
+	// LOGGING: use the LOG(...) macro
 	//LOG(3, "Execution of " << info.id << "!");
 
 	// VARIOUS INFORMATION FROM BVS
-	/*
-	unsigned long long round = bvs.round;
-	int lastRoundModuleDuration = bvs.moduleDurations.find(info.id)->second.count();
-	int lastRoundDuration = bvs.lastRoundDuration.count();
-	*/
+	//unsigned long long round = bvs.round;
+	//int lastRoundModuleDuration = bvs.moduleDurations.find(info.id)->second.count();
+	//int lastRoundDuration = bvs.lastRoundDuration.count();
 
-	// SIMPLE CONNECTOR EXAMPLE (it is always a good idea to check input, twice)
-	/*
-	int incoming;
-	if (!input.receive(incoming)) return BVS::Status::NOINPUT;
-	if (incoming==int()) return BVS::Status::NOINPUT;
-
-	std::string message = "received " + std::to_string(incoming);
-	output.send(message);
-	*/
-
-	// ADVANCED CONNECTOR EXAMPLE (not unlocking the connection will cause deadlock!)
-	/*
-	output.lockConnection();
-	*output = "This";
-	*output = *output + " is an";
-	*output += "example!";
-	output.unlockConnection();
-	*/
+	// CONNECTOR USAGE: it is always a good idea to check input, twice
+	//int incoming;
+	//if (!input.receive(incoming)) return BVS::Status::NOINPUT;
+	//if (incoming==int()) return BVS::Status::NOINPUT;
+	//std::string message = "received " + std::to_string(incoming);
+	//output.send(message);
 
 	return BVS::Status::OK;
 }
