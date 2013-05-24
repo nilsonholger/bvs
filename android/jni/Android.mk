99fd136867f2eb57da93dbd129fcafeccf4859da
Benjamin Sigrist - Studienarbeit - Betreuer Daniel Koester <bsigrist@i14pc186.anthropomatik.kit.edu>
2013-05-24 13:14:24 +0200

    android:outsourced opencv path , -llog for modules

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -1,8 +1,13 @@
 BVS_OPTIONS:=-DBVS_LOG_SYSTEM
+#LOCAL_PATH variable is needed from buildsystem
 LOCAL_PATH := $(abspath $(call my-dir)/../../)
+BVS_ROOT_PATH := $(LOCAL_PATH)
+#get location of local OPENCV.MK from seperated mk
+include $(BVS_ROOT_PATH)/android/local.opencv.mk
+OPENCVMK:=$(LOCAL_OPENCV)
 #BvsA.h need opencv, but we cant link against it, because ...
 #so save the LOCAL_C_INCLUDES to a variable and CLEAR all LOCAL_Variable
-include $(LOCAL_PATH)/../opencvsdk/native/jni/OpenCV.mk
+include $(OPENCVMK)
 OPENCV_C_INCLUDES:=$(LOCAL_C_INCULDES)
 
 include $(CLEAR_VARS)
@@ -15,7 +20,7 @@ LOCAL_MODULE    := BvsA
 
 include $(call all-subdir-makefiles)
 	
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/lib/include 
+LOCAL_C_INCLUDES :=$(BVS_ROOT_PATH)/lib/include 
 LOCAL_C_INCLUDES += /usr/local/include
 LOCAL_SRC_FILES :=lib/src/barrier.cc
 LOCAL_SRC_FILES +=lib/src/bvs.cc
@@ -31,9 +36,8 @@ LOCAL_SRC_FILES +=lib/src/module.cc
 LOCAL_SRC_FILES +=lib/src/utils.cc
 
 LOCAL_SRC_FILES += android/jni/BvsA.cpp
-
-LOCAL_LDLIBS  += -llog -ldl 
-#LOCAL_LDLIBS += $(LOCAL_PATH)/../../opencvsdk/native/libs/armeabi-v7a/libopencv_core.a -L/home/bsigrist/local/lib/libopencv_core.so
+# --llog for logging
+LOCAL_LDLIBS  += -llog 
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -45,14 +49,15 @@ include $(CLEAR_VARS)
 #OPENCV_INSTALL_MODULES:=on
 #OPENCV_CAMERA_MODULES:=on
 #OPENCV_LIB_TYPE:=STATIC
-include $(LOCAL_PATH)/../opencvsdk/native/jni/OpenCV.mk
+include $(BVS_ROOT_PATH)/../opencvsdk/native/jni/OpenCV.mk
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/lib/include 
+LOCAL_C_INCLUDES += $(BVS_ROOT_PATH)/lib/include 
 LOCAL_CPPFLAGS := -std=c++11 -pthread -frtti -fexceptions $(BVS_OPTIONS)
 APP_USE_CPP0X := true 
 
 LOCAL_ARM_MODE := arm
-
+# --llog for logging
+LOCAL_LDLIBS  += -llog 
 LOCAL_MODULE    := ExampleCV
 LOCAL_SRC_FILES := modules/bvs-modules/ExampleCV/ExampleCV.cc
 
@@ -66,14 +71,16 @@ include $(CLEAR_VARS)
 #OpenCV
 ##### Following line not needed if OpenCV_Manager available on device
 #OPENCV_LIB_TYPE:=STATIC
-include $(LOCAL_PATH)/../opencvsdk/native/jni/OpenCV.mk
+include $(BVS_ROOT_PATH)/../opencvsdk/native/jni/OpenCV.mk
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/lib/include 
+LOCAL_C_INCLUDES += $(BVS_ROOT_PATH)/lib/include 
+LOCAL_C_INCLUDES += $(BVS_ROOT_PATH)/android
 LOCAL_CPPFLAGS := -std=c++11 -pthread -frtti -fexceptions $(BVS_OPTIONS)
 APP_USE_CPP0X := true 
 
 LOCAL_ARM_MODE := arm
 
+LOCAL_LDLIBS  += -llog 
 LOCAL_MODULE    := AndroidCV
 LOCAL_SRC_FILES := modules/bvs-modules/AndroidCV/AndroidCV.cc
 LOCAL_SHARED_LIBRARIES := BvsA
