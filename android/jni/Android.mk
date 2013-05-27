a4304338bd6b5784df6a5efb2c6b9bc62cbe3876
Benjamin Sigrist - Studienarbeit - Betreuer Daniel Koester <bsigrist@i14pc186.anthropomatik.kit.edu>
2013-05-27 11:44:16 +0200

    android: Modules have their own android.mk now

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -3,13 +3,17 @@ BVS_OPTIONS:=-DBVS_LOG_SYSTEM
 #LOCAL_PATH variable is needed from buildsystem
 LOCAL_PATH := $(abspath $(call my-dir)/../../)
 BVS_ROOT_PATH := $(LOCAL_PATH)
+BVS_MODULES_PATH:= $(BVS_ROOT_PATH)/modules/bvs-modules
+$(warning $(BVS_MODULES_PATH)...)
+
 #get location of local OPENCV.MK from seperated mk in variable LOCAL_OPENCV
 include $(BVS_ROOT_PATH)/android/local.opencv.mk
 #BvsA.h need opencv, but we cant link against it, linked against it twice
 #so save the LOCAL_C_INCLUDES to OPENCV_C_INCLUDES  and CLEAR_VARS
 include $(LOCAL_OPENCV)
-OPENCV_C_INCLUDES:=$(LOCAL_C_INCLUDES)
+#OPENCV_C_INCLUDES:=$(LOCAL_C_INCLUDES)
 
+OPENCV_C_INCLUDES:=$(OPENCV_LOCAL_C_INCLUDES)
 include $(CLEAR_VARS)
 
 LOCAL_CPPFLAGS := -std=c++11 -pthread -frtti -fexceptions $(BVS_OPTIONS)
@@ -42,48 +46,14 @@ LOCAL_LDLIBS  += -llog
 
 include $(BUILD_SHARED_LIBRARY)
 
+#ANDROIDCV MODULE
 include $(CLEAR_VARS)
-
-
-#OpenCV
-##### Following lines not needed if OpenCV_Manager available on device
-#OPENCV_INSTALL_MODULES:=on
-#OPENCV_CAMERA_MODULES:=on
-#OPENCV_LIB_TYPE:=STATIC
-include $(LOCAL_OPENCV)
-
-LOCAL_C_INCLUDES += $(BVS_ROOT_PATH)/lib/include 
-LOCAL_CPPFLAGS := -std=c++11 -pthread -frtti -fexceptions $(BVS_OPTIONS)
-APP_USE_CPP0X := true 
-
-LOCAL_ARM_MODE := arm
-# --llog for logging
-LOCAL_LDLIBS  += -llog 
-LOCAL_MODULE    := ExampleCV
-LOCAL_SRC_FILES := modules/bvs-modules/ExampleCV/ExampleCV.cc
-
-LOCAL_SHARED_LIBRARIES := BvsA
-
+include $($(BVS_MODULES_PATH)/AndroidCV/Android.mk)
+LOCAL_MODULE    := AndroidCV
 include $(BUILD_SHARED_LIBRARY)
 
-#AndroidCV
+#AndroidCV MODULE
 include $(CLEAR_VARS)
-
-#OpenCV
-##### Following line not needed if OpenCV_Manager available on device
-#OPENCV_LIB_TYPE:=STATIC
-include $(LOCAL_OPENCV)
-
-LOCAL_C_INCLUDES += $(BVS_ROOT_PATH)/lib/include 
-LOCAL_C_INCLUDES += $(BVS_ROOT_PATH)/android
-LOCAL_CPPFLAGS := -std=c++11 -pthread -frtti -fexceptions $(BVS_OPTIONS)
-APP_USE_CPP0X := true 
-
-LOCAL_ARM_MODE := arm
-
-LOCAL_LDLIBS  += -llog 
-LOCAL_MODULE    := AndroidCV
-LOCAL_SRC_FILES := modules/bvs-modules/AndroidCV/AndroidCV.cc
-LOCAL_SHARED_LIBRARIES := BvsA
-
+include $($(BVS_MODULES_PATH)/ExampleCV/ExampleCV.mk)
+LOCAL_MODULE    := ExampleCV
 include $(BUILD_SHARED_LIBRARY)
