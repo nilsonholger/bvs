655b0e0099aaa63b179cfde5e71a827662361bdf
nilsonholger <nilsonholger@hyve.org>
2015-05-19 13:26:06 +0200

    cmake: improve setting descriptions

--- cmake/bvs-options.cmake
+++ cmake/bvs-options.cmake
@@ -6,26 +6,26 @@ set(BVS_ANDROID_APP FALSE CACHE BOOL "Generate Android targets!")
 mark_as_advanced(BVS_ANDROID_APP)
 
 # BVS_GCC_VISIBILITY
-set(BVS_GCC_VISIBILITY ON CACHE BOOL "Enable gcc's visibility feature (reduce dynamic library load times as well as decrease library size).")
+set(BVS_GCC_VISIBILITY ON CACHE BOOL "Enable GCC's visibility feature (reduce library size and load time).")
 mark_as_advanced(BVS_GCC_VISIBILITY)
 
 # BVS_LOG_SYSTEM
-set(BVS_LOG_SYSTEM ON CACHE BOOL "Enable BVS builtin log system.")
+set(BVS_LOG_SYSTEM ON CACHE BOOL "Enable BVS' builtin log system.")
 
 # BVS_MODULE_HOTSWAP
-set(BVS_MODULE_HOTSWAP OFF CACHE BOOL "Enable BVS's HotSwap(TM;-) facilities. WARNING: DEVELOPERS ONLY!")
+set(BVS_MODULE_HOTSWAP OFF CACHE BOOL "Enable BVS's modular HotSwap(TM;-) facilities. WARNING: DEVELOPERS ONLY!")
 mark_as_advanced(BVS_MODULE_HOTSWAP)
 
 # BVS_STATIC_MODULES
-set(BVS_STATIC_MODULES OFF CACHE BOOL "Enable compilation of static modules and creation of single library object!")
+set(BVS_STATIC_MODULES OFF CACHE BOOL "Include modules in 'libbvs'!")
 mark_as_advanced(BVS_STATIC_MODULES)
 
 # BVS_THREAD_NAMES
-set(BVS_THREAD_NAMES ${UNIX} CACHE BOOL "Enable thread naming (htop: enable 'Show custom thread names', or try 'ps -La'). UNIX ONLY!")
+set(BVS_THREAD_NAMES ${UNIX} CACHE BOOL "Name threads (htop: enable 'Show custom thread names', or try 'ps -La'). UNIX ONLY!")
 mark_as_advanced(BVS_THREAD_NAMES)
 
 # BVS_OSX_ANOMALIES
-set(BVS_OSX_ANOMALIES ${APPLE} CACHE BOOL "Enable OSX anomalies, e.g. 'so->dylib'.")
+set(BVS_OSX_ANOMALIES ${APPLE} CACHE BOOL "Deal with OSX anomalies, e.g. 'so->dylib'.")
 mark_as_advanced(BVS_OSX_ANOMALIES)
 
 
